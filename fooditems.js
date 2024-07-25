0b9ff37c7e2100c138b4b7d222250d8dd3185682
gopika <gopika@gmail.com>
2024-07-25 19:07:53 +0530

    initial commit

---
+++ fooditems.js
@@ -0,0 +1,187 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Ambur Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'images/biryani/Ambur-Chicken-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/Chicken-Biryani-hyd.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 18,
+    img: 'images/biryani/egg-biryani.jpeg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Goan Fish Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/goan-fish-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 10,
+    img: 'images/biryani/hyd-Mutton-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Kamrupi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 12,
+    img: 'images/biryani/kamrupi-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'images/biryani/kashmiri.pulao.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Memoni Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 20,
+    img: 'images/biryani/memonibiryani.png',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/mughlai-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chicken Roast',
+    category : 'chicken',
+    rating : 4.3,
+    price: 11,
+    img: 'images/chicken/Chicken_roast.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 10,
+    img: 'images/chicken/Chicken-Curry.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Chicken Do Pyaza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 14,
+    img: 'images/chicken/Chicken-do-Pyaza.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Chicken Masala',
+    category : 'chicken',
+    rating : 4.3,
+    price: 12,
+    img: 'images/chicken/Chicken-Masala.jpeg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Handi Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 17,
+    img: 'images/chicken/Handi-chicken.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Murgh Musallam',
+    category : 'chicken',
+    rating : 4.3,
+    price: 20,
+    img: 'images/chicken/Murgh-Musallam.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/paneer/Matar-Paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 10,
+    img: 'images/paneer/palak-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Paneer Butter Masala',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/paneer/paneer-butter-masala.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Paneer Do Pyaza',
+    category : 'paneer',
+    rating : 4.3,
+    price: 12,
+    img: 'images/paneer/Paneer-Do-Pyaza.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Hyderabadi Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 8,
+    img: 'images/paneer/Paneer-Hyderabadi.jpg',
+    quantity: 1
+}
+]
+
+export {foodItem};
